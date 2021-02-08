02f87c7f99276e3cca1802fd870aec75eae72ea5
Krenil <krenilbhikadiya2000@gmail.com>
2021-02-08 17:50:49 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "23238179c0b60c1d7ce7f00106ce2060"
+    }
+}
+
+module.exports = constants;
